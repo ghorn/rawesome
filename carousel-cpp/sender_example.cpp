c5b77c164edbdcdfed512e8153b71e8ce4462d03
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2012-10-16 18:34:21 +0200

    Writing sender_example.cpp

--- carousel-cpp/sender_example.cpp
+++ carousel-cpp/sender_example.cpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <stdint.h>
 
 #include "kite.pb.h"
@@ -9,4 +10,18 @@
 using namespace std;
 int main(int argc, char **argv) 
 {
+	GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+	kite::CarouselState cs;
+
+	{
+		// Write the kite state to disk.
+		fstream output("sample_kite_message", ios::out | ios::trunc | ios::binary);
+		if (!cs.SerializeToOstream(&output)) {
+			cerr << "Failed to write sample_kite_message to disk." << endl;
+			return -1;
+		}
+	}
+
+	google::protobuf::ShutdownProtobufLibrary(); // optional
 }
