133835688e9edb3cf836533260d4cc40c2246894
Greg Horn <gregmainland@gmail.com>
2012-10-24 13:36:35 +0200

    remove deprecated haskell carousel-opt and carousel-sim, rename carousel-py to rawe

--- mkprotos.sh
+++ mkprotos.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p carousel-cpp
-protoc --python_out=carousel-py --cpp_out=carousel-cpp kite.proto
+protoc --python_out=rawe --cpp_out=carousel-cpp kite.proto
 
 cd kitevis
 hprotoc -I.. --haskell_out=src kite.proto
