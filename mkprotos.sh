4b41931493da96fc2ff6a98fbd2392ecb114d6b1
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2012-10-29 18:32:42 +0100

    Improved error reporting of mkprotos.sh

--- mkprotos.sh
+++ mkprotos.sh
@@ -3,11 +3,17 @@
 mkdir -p carousel-cpp
 protoc --python_out=rawe --cpp_out=carousel-cpp kite.proto
 
-cd kitevis
-hprotoc -I.. --haskell_out=src kite.proto
-cd ..
-
-cd trajectory-gui
-hprotoc -I.. --haskell_out=src kite.proto
-cd ..
-
+if [ -x hprotoc ]
+then
+	(
+	cd kitevis
+	hprotoc -I.. --haskell_out=src kite.proto
+	)
+	(
+	cd trajectory-gui
+	hprotoc -I.. --haskell_out=src kite.proto
+	)
+else
+	echo "Not generating haskell protobuf interface since hprotoc not installed;"
+	echo 'Try running "cabal install hprotoc"'
+fi
