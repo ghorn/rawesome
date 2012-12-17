4deef1deeb734bea6d96e0b8e121ace799dd682c
Greg Horn <gregmainland@gmail.com>
2012-12-17 18:17:33 +0100

    added basic Nmpc class

--- rawe/newton/newton.py
+++ rawe/newton/newton.py
@@ -1,5 +1,101 @@
 import casadi as C
+import numpy as np
 
+class NmpcMap(object):
+    def __init__(self,dae,nk,X,U,p):
+        self._nk = nk
+        self._xNames = dae.xNames()
+        self._uNames = dae.uNames()
+        self._pNames = dae.pNames()
+
+        self._X = X
+        self._U = U
+        self._p = p
+        
+        self._xIdx = {}
+        self._uIdx = {}
+        self._pIdx = {}
+        for k,name in enumerate(self._xNames):
+            self._xIdx[name] = k
+        for k,name in enumerate(self._uNames):
+            self._uIdx[name] = k
+        for k,name in enumerate(self._pNames):
+            self._pIdx[name] = k
+
+    def vectorize(self):
+        return C.veccat([self._X,self._U,self._p])
+        # on exception try
+        return np.append(self._X.flatten(),self._U.flatten(),self._p.flatten())
+    
+    def lookup(self,name,timestep=None):
+        if name in self._xIdx:
+            assert (timestep != None), "please set timestep"
+            assert (timestep <= self._nk), "timestep too large"
+            return self._X[self._xIdx[name],timestep]
+        elif name in self._uIdx:
+            assert (timestep != None), "please set timestep"
+            assert (timestep < self._nk), "timestep too large"
+            return self._U[self._uIdx[name],timestep]
+        elif name in self._pIdx:
+            assert (timestep == None), "don't set timestep for parameter"
+            return self._p[self._pIdx[name]]
+        else:
+            raise NameError('unrecognized name "'+name+'"')
+
+    def setVal(self,name,val,timestep=None):
+        if name in self._xIdx:
+            assert (timestep != None), "please set timestep"
+            assert (timestep <= self._nk), "timestep too large"
+            self._X[self._xIdx[name],timestep] = val
+        elif name in self._uIdx:
+            assert (timestep != None), "please set timestep"
+            assert (timestep < self._nk), "timestep too large"
+            self._U[self._uIdx[name],timestep] = val
+        elif name in self._pIdx:
+            assert (timestep == None), "don't set timestep for parameter"
+            self._p[self._pIdx[name]] = val
+        else:
+            raise NameError('unrecognized name "'+name+'"')
+
+class Nmpc(object):
+    def __init__(self,dae,nk):
+        self.dae = dae
+        self.nk = nk
+
+        X = C.ssym('x',dae.xVec().size(),self.nk+1)
+        U = C.ssym('u',dae.zVec().size(),self.nk)
+        p = C.ssym('p',dae.pVec().size())
+        self._dvMap = NmpcMap(self.dae,self.nk,X,U,p)
+
+        X = np.resize(np.array([None]),(dae.xVec().size(),self.nk+1))
+        U = np.resize(np.array([None]),(dae.zVec().size(),self.nk))
+        p = np.resize(np.array([None]),dae.pVec().size())
+        self._boundMap = NmpcMap(self.dae,self.nk,X,U,p)
+
+        X = np.resize(np.array([None]),(dae.xVec().size(),self.nk+1))
+        U = np.resize(np.array([None]),(dae.zVec().size(),self.nk))
+        p = np.resize(np.array([None]),dae.pVec().size())
+        self._guessMap = NmpcMap(self.dae,self.nk,X,U,p)
+
+    def __call__(self,*args,**kwargs):
+        return self.lookup(*args,**kwargs)
+
+    def lookup(self,name,timestep=None):
+        return self._dvMap.lookup(name,timestep=timestep)
+
+    def bound(self,name,(lb,ub),timestep=None):
+        self._boundMap.setVal(name,(lb,ub),timestep=timestep)
+        
+#    def setObj(self,obj):
+#        if hasattr(self,'_obj'):
+#            raise ValueError("don't change the objective function")
+#        self._obj = obj
+
+    def setGaussNewtonObjF(self,gnF):
+        if hasattr(self,'_gnF'):
+            raise ValueError("don't change the objective function")
+        self._gnF = gnF
+        
 class Newton(object):
     def __init__(self,LagrangePoly,dae,nk,nicp,deg,collPoly):
         self.dae = dae
---
+++ rawe/runmhe.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+import casadi as C
+
+from collocation import Coll,LagrangePoly
+import models
+from newton.newton import Newton,Nmpc
+
+if __name__ == '__main__':
+    print "creating model"
+    dae = models.pendulum2()
+#    dae.addP('endTime')
+
+    nk = 30
+    nicp = 1
+    deg = 4
+    newton = Newton(LagrangePoly,dae,nk,nicp,deg,'RADAU')
+    endTime = 0.05*nk
+    newton.setupStuff(endTime)
+
+    r = 0.3
+    x0 = [r,0,0,0]
+
+    xTraj = [np.array(x0)]
+    uTraj = []
+    p = np.array([0.3])
+    
+    # make a trajectory
+    for k in range(nk):
+        uTraj.append(np.array([5*C.sin(k/float(nk))]))
+        newton.isolver.setInput(xTraj[-1],0)
+        newton.isolver.setInput(uTraj[-1],1)
+        newton.isolver.setInput(p[-1],2)
+        newton.isolver.evaluate()
+        xTraj.append(np.array(newton.isolver.output(1)).squeeze())
+
+    for k in range(nk):
+        print xTraj[k],uTraj[k]
+    print xTraj[-1]
+
+    # setup MHE
+    ocp = Nmpc(dae,nk)
+
+    # make objective
+    obj = 0
+    for k in range(nk):
+        obj += (ocp('x',timestep=k) - xTraj[k][0])**2
+        obj += (ocp('z',timestep=k) - xTraj[k][1])**2
+        obj += (ocp('dx',timestep=k) - xTraj[k][2])**2
+        obj += (ocp('dz',timestep=k) - xTraj[k][3])**2
+    print obj
+
+
+
+##    newton.isolver.input(0).set(x0)
+#    newton.isolver.setInput(x0,0)
+#    newton.isolver.setInput(0,1) # torque
+#    newton.isolver.setInput(0.3,2) # mass
+#    newton.isolver.evaluate()
+#    newton.isolver.evaluate()
+#
+#    j = newton.isolver.jacobian(0,1)
+#    j.init()
+#    j.setInput(x0,0)
+#    j.setInput(0,1)
+#    j.setInput(0.3,2)
+#    j.evaluate()
+#    print j.output(0)
+#    print j.output(1)
