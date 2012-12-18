800212b2b85ea78f6def44c3b57becbd4c54c650
Greg Horn <gregmainland@gmail.com>
2012-12-18 15:47:37 +0100

    add constraints and obj to nmpc

--- rawe/newton/multipleShooting.py
+++ rawe/newton/multipleShooting.py
@@ -3,11 +3,14 @@ import numpy as np
 import casadi as C
 
 import msmaps
+from ocputils import Constraints
 
 class Nmpc(object):
     def __init__(self,dae,nk):
         self.dae = dae
         self.nk = nk
+
+        self._gaussNewtonObjF = []
 
         X = C.ssym('x',dae.xVec().size(),self.nk+1)
         U = C.ssym('u',dae.zVec().size(),self.nk)
@@ -27,6 +30,8 @@ class Nmpc(object):
         self._outputMapGenerator = msmaps.OutputMapGenerator(self)
         self._outputMap = msmaps.OutputMap(self._outputMapGenerator, self._dvMap.vectorize())
 
+        self._constraints = Constraints()
+        
     def __call__(self,*args,**kwargs):
         return self.lookup(*args,**kwargs)
 
@@ -44,13 +49,15 @@ class Nmpc(object):
 
     def bound(self,name,(lb,ub),timestep=None):
         self._boundMap.setVal(name,(lb,ub),timestep=timestep)
-        
-#    def setObj(self,obj):
-#        if hasattr(self,'_obj'):
-#            raise ValueError("don't change the objective function")
-#        self._obj = obj
 
-    def setGaussNewtonObjF(self,gnF):
-        if hasattr(self,'_gnF'):
+    def constrain(self,lhs,comparison,rhs,tag='unnamed_constraint'):
+        self._constraints.add(lhs,comparison,rhs,tag)
+        
+    def setObj(self,obj):
+        if hasattr(self,'_obj'):
             raise ValueError("don't change the objective function")
-        self._gnF = gnF
+        self._obj = obj
+
+    def addGaussNewtonObjF(self,gnF):
+        self._gaussNewtonObjF.append(gnF)
+
