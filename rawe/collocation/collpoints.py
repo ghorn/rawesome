c6f398f89540c6bffd172465f22ec8a2eacdc220
Greg Horn <gregmainland@gmail.com>
2013-01-29 08:50:52 +0100

    clean up collocation points code

--- rawe/collocation/collpoints.py
+++ rawe/collocation/collpoints.py
@@ -2,22 +2,22 @@ import scipy.special
 import numpy as np
 
 def mkCollocationPoints(collPoly,deg):
-    assert(deg>0)
+#    assert(deg>0)
     # Legendre collocation points
-    legendre_points1 = [0,0.500000]
-    legendre_points2 = [0,0.211325,0.788675]
-    legendre_points3 = [0,0.112702,0.500000,0.887298]
-    legendre_points4 = [0,0.069432,0.330009,0.669991,0.930568]
-    legendre_points5 = [0,0.046910,0.230765,0.500000,0.769235,0.953090]
-    legendre_points = [[0],legendre_points1,legendre_points2,legendre_points3,legendre_points4,legendre_points5]
+    legendre_points = [[0],
+                       [0,0.500000],
+                       [0,0.211325,0.788675],
+                       [0,0.112702,0.500000,0.887298],
+                       [0,0.069432,0.330009,0.669991,0.930568],
+                       [0,0.046910,0.230765,0.500000,0.769235,0.953090]]
     
     # Radau collocation points
-    radau_points1 = [0,1.000000]
-    radau_points2 = [0,0.333333,1.000000]
-    radau_points3 = [0,0.155051,0.644949,1.000000]
-    radau_points4 = [0,0.088588,0.409467,0.787659,1.000000]
-    radau_points5 = [0,0.057104,0.276843,0.583590,0.860240,1.000000]
-    radau_points = [[0],radau_points1,radau_points2,radau_points3,radau_points4,radau_points5]
+    radau_points = [[0],
+                    [0,1.000000],
+                    [0,0.333333,1.000000],
+                    [0,0.155051,0.644949,1.000000],
+                    [0,0.088588,0.409467,0.787659,1.000000],
+                    [0,0.057104,0.276843,0.583590,0.860240,1.000000]]
 
     hardCoded = None
     if collPoly == 'LEGENDRE':
@@ -30,10 +30,11 @@ def mkCollocationPoints(collPoly,deg):
         alpha = 1.0
         beta = 0.0
         if deg > 1:
-            roots = 0.5*(scipy.special.j_roots(deg-1,alpha,beta)[0] + 1)
+            roots = np.append(0.5*(scipy.special.j_roots(deg-1,alpha,beta)[0] + 1),1.0)
+        elif deg == 1:
+            roots = np.array([1.0])
         else:
             roots = np.array([])
-        roots = np.append(roots,1.0)
         if deg<6:
             hardCoded = radau_points[deg]
     else:
