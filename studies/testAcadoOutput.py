b7f7449c892fafba4158ab7fb1a23daa4c9cd7ae
Greg Horn <gregmainland@gmail.com>
2013-03-25 12:09:04 +0100

    testAcadoOutput now generates a rien integrator

--- studies/testAcadoOutput.py
+++ studies/testAcadoOutput.py
@@ -5,36 +5,30 @@ import casadi as C
 import rawe
 
 if __name__=='__main__':
-    from conf import conf
+    from highwind_carousel_conf import conf
     
     print "creating model..."
     dae = rawe.models.carousel(conf)
     
-    blah = dae.octaveSimGen('carouselOde')
-    f = open('carouselOde_modelAndJacob.m','w')
-    f.write(blah)
-    f.close()
-#    (modelFile, simExportFile) = 
-#    print modelFile
-#    print simExportFile
-    import sys; sys.exit()
+#    blah = dae.octaveSimGen('carouselOde')
+#    f = open('carouselOde_modelAndJacob.m','w')
+#    f.write(blah)
+#    f.close()
 
     (modelFile, simExportFile) = dae.acadoSimGen()
-    print modelFile
-    print simExportFile
 
-    f = open('auto_model.c','w')
+    f = open('data/auto_model.c','w')
     f.write(modelFile)
     f.close()
 
-    f = open('auto_sim_export.c','w')
+    f = open('data/auto_sim_export.c','w')
     f.write(simExportFile)
     f.close()
+#
+#    modelFile = dae.acadoModelGen()
+#    print modelFile
+#    print simExportFile
 
-    modelFile = dae.acadoModelGen()
-    print modelFile
-    print simExportFile
-
-    f = open('auto_model.cpp','w')
-    f.write(modelFile)
-    f.close()
+#    f = open('auto_model.cpp','w')
+#    f.write(modelFile)
+#    f.close()
