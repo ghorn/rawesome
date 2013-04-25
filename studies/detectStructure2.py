1ad1fbea8dd2918e28adff7f96ae17d885aa7cee
Greg Horn <gregmainland@gmail.com>
2013-04-25 17:15:09 +0200

    add linear output detection

--- studies/detectStructure2.py
+++ studies/detectStructure2.py
@@ -2,14 +2,8 @@ import rawe
 import casadi as C
 
 if __name__=='__main__':
-    print "creating model..."
     from highwind_carousel_conf import conf
     dae = rawe.models.carousel(conf)
-
-    print 'x',dae.xNames()
-    print 'z',dae.zNames()
-    print 'u',dae.uNames()
-    print 'p',dae.pNames()
 
     f = dae.getResidual()
     xdot = C.veccat([dae.ddt(name) for name in dae.xNames()])
@@ -21,13 +15,7 @@ if __name__=='__main__':
     nz = z.size()
     nup = u.size() + p.size()
 
-    print f.shape
-    print 
     inputs = C.veccat([x, z, u, p, xdot])
-    xIdx = range(0,nx)
-    zIdx = range(nx,nx+nz)
-    upIdx = range(nx+nz,nx+nz+nup)
-    xDotIdx = range(nx+nz+nup,nx+nz+nup+nx)
 
     # take jacobian and find which entries are constant, zero, or nonzer
     jac = C.jacobian(f,inputs)
@@ -71,7 +59,7 @@ if __name__=='__main__':
             if MA[i,j] == 2 or MC[i,j] == 2:
                 raise Exception('the "impossible" happend')
 #            if MC[i,j] == 1 or MA[i,j] == 1:
-            if MC[i,j] == 1:
+            if MC[i,j] == 1: # poor man's C1 square constraint
                 xj_linear.add(j)
 
     # which variables are in the nonlinear set
@@ -81,18 +69,8 @@ if __name__=='__main__':
             if MA[i,j] >= 1 or MC[i,j] >= 1:
                 xj_nonlinear.add(j)
 
-    print fi_nonlinear
-    print fi_linear
-    print [dae.xNames()[j] for j in xj_nonlinear]
-    print [dae.xNames()[j] for j in xj_linear]
-    print "="*80
     x23_candidates = xj_nonlinear - xj_linear
     x1_candidates = set(range(nx))-x23_candidates
-    print "x23_c",[dae.xNames()[j] for j in x23_candidates]
-    print "x1_c",[dae.xNames()[j] for j in x1_candidates]
-
-    for k in fi_linear:
-        print f[k]
 
     # kick out linear variables which depend on nonlinear ones
     changed = True
@@ -100,17 +78,16 @@ if __name__=='__main__':
     while changed == True:
         niters += 1
         changed = False
+        badRows = set()
         for i in fi_linear:
-            badColumn = False
             if any(MZ[i,:] > 0):
                 raise Exception('the "impossible" happened')
             for j in x23_candidates:
                 if MA[i,j] > 0 or MC[i,j] > 0:
-                    badColumn = True
+                    badRows.add(i)
             
             # oh shit, a tainted row, blow away everything here
-            if badColumn:
-                print 'got a bad column, yo'
+            if i in badRows:
                 removeUs = set()
                 for j in x1_candidates:
                     if MA[i,j] > 0 or MC[i,j] > 0:
@@ -119,7 +96,41 @@ if __name__=='__main__':
                 for j in removeUs:
                     x1_candidates.remove(j)
                     x23_candidates.add(j)
+        for i in badRows:
+            fi_nonlinear.add(i)
+            fi_linear.remove(i)
+    f1 = fi_linear
+    f23 = fi_nonlinear
+    x1 = x1_candidates
+    x23 = x23_candidates
     print "finished in",niters,"iterations"
-    print "-"*80
-    print "x23_c",[dae.xNames()[j] for j in x23_candidates]
-    print "x1_c",[dae.xNames()[j] for j in x1_candidates]
+    print "LOL HERE IS WHERE WE CHECK IF C1 IS SQUARE (yolo)"
+
+    # separate x23 into x2 and x3
+    x3_candidates = set(x23)
+    for i in f23:
+        not_x3 = set()
+        for j in x3_candidates:
+            if MC[i,j] == 2 or MA[i,j] == 2:
+                not_x3.add(j)
+        for j in not_x3:
+            x3_candidates.remove(j)
+    x3 = x3_candidates
+    x2 = x23 - x3
+
+    # separate f23 into f2 and f3
+    not_f3 = set()
+    for i in f23:
+        for j in x3:
+            if MA[i,j] > 0 or MC[i,j] > 0:
+                not_f3.add(i)
+    f2 = f23 - not_f3
+    f3 = f23 - f2
+
+    print "LOL HERE IS WHERE WE CHECK IF C3 IS SQUARE (yolo)"
+    print "x1",[dae.xNames()[j] for j in x1]
+    print "x2",[dae.xNames()[j] for j in x2]
+    print "x3",[dae.xNames()[j] for j in x3]
+    print "f1",f1
+    print "f2",f2
+    print "f3",f3
