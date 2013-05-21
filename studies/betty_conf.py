89eea03dcf98467d6c54080a21409cbcd238d9ac
Greg Horn <gregmainland@gmail.com>
2013-05-21 19:30:11 +0200

    add betty configuration

---
+++ studies/betty_conf.py
@@ -0,0 +1,73 @@
+conf = {#[env]
+     'g': 9.81,  #  gravitational constant #  [ m /s^2]
+     'rho': 1.23,  #  density of the air #  [ kg/m^3]
+
+     #[aero]
+     'alpha0deg': 0,
+
+     #ROLL DAMPING
+     'rD': 0.3e-2 ,
+     'pD': 0,#1e-3,
+     'yD': 0,#1e-3,
+
+     #WIND-TUNNEL PARAMETERS
+     #Lift (report p. 67)
+     'cLA': 5.786876,
+     'cLe': -1.924*0,
+     'cL0': 0.203530,
+
+     #Drag (report p. 70)
+     'cDA': 0.018751,
+     'cDA2': 1.529989,
+     'cDB2': 0,
+
+     'cD0': 0.006952,
+
+     #Roll (report p. 72)
+     'cRB': -0.062,
+     'cRAB': -0.271 ,
+     'cRr': -5.637e-1,
+
+     #Pitch (report p. 74)
+     'cPA': 0.293,
+     'cPe': -4.9766e-1,
+
+     'cP0': 0.03,
+
+     #Yaw (report p. 76)
+     'cYB': 0.05,
+     'cYAB': 0.229,
+
+     #[kite]
+     'mass':  4.5,
+
+     #TAIL LENGTH
+     'lT': 0.4,
+
+     # how alpha/beta computed
+     'alpha_beta_computation':'closed_form',
+
+     'sref': 0.684,
+     'bref': 2.904, #sqrt(sref*AR),
+     'cref': 0.2512, #sqrt(sref/AR),
+
+     'zt': -0.01,
+
+     #INERTIA MATRIX (Kurt's direct measurements)
+     'j1': 0.0163,
+    'j31': 0.0006,
+    'j2': 0.0078,
+    'j3': 0.0229,
+    
+    #[carousel]
+    #'rArm': 1.085 #(dixit Kurt),
+    'rArm': 2,
+    
+    #Carousel Friction & inertia
+    'jCarousel': 1e2,
+    'cfric': 0,#100,
+    
+    #[wind shear]
+    'z0': 100,
+    'zt_roughness': 0.1
+    }
--- wtfviz/src/DrawAC.hs
+++ wtfviz/src/DrawAC.hs
@@ -12,7 +12,7 @@ drawAc :: Float -> Xyz Double -> Quat Double -> (VisObject Double, [Xyz Double])
 drawAc alpha pos quat = (VisObjects $ wing ++ [htail,vtail,body], vtip:wingtips)
   where
 --    axes = Trans pos $ RotQuat quat $ Axes (0.5, 15)
-    spanW = 1
+    spanW = 2.9
     arW = 9
     
     spanHRatio = 0.4
