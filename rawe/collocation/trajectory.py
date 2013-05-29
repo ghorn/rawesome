67e5a0d5f6beed0bbf4ef9661000caee00380138
Greg Horn <gregmainland@gmail.com>
2013-05-29 12:31:37 +0200

    trajectory can now save .mat files

--- rawe/collocation/trajectory.py
+++ rawe/collocation/trajectory.py
@@ -17,7 +17,8 @@
 
 import matplotlib.pyplot as plt
 import pickle
-import numpy as np
+import numpy
+import scipy.io
 
 import casadi as C
 import collmaps
@@ -43,11 +44,11 @@ class Trajectory(object):
         ocp.hfun.evaluate()
         h = float(ocp.hfun.output())
 
-        self.tgrid = np.resize([],(ocp.nk+1,ocp.nicp,ocp.deg+1))
+        self.tgrid = numpy.resize([],(ocp.nk+1,ocp.nicp,ocp.deg+1))
         tf = 0.0
         for k in range(ocp.nk):
             for i in range(ocp.nicp):
-                self.tgrid[k,i,:] = tf + h*np.array(ocp.lagrangePoly.tau_root)
+                self.tgrid[k,i,:] = tf + h*numpy.array(ocp.lagrangePoly.tau_root)
                 tf += h
         self.tgrid[ocp.nk,0,0] = tf
 
@@ -59,11 +60,11 @@ class Trajectory(object):
             return self.dvMap.lookup(name,timestep=timestep,nicpIdx=nicpIdx,degIdx=degIdx)
         except NameError:
             pass
-        try: 
+        try:
             return self.outputMap.lookup(name,timestep=timestep,nicpIdx=nicpIdx,degIdx=degIdx)
         except NameError:
             pass
-        try: 
+        try:
             return self.quadratureMap.lookup(name,timestep,nicpIdx,degIdx)
         except NameError:
             pass
@@ -77,6 +78,111 @@ class Trajectory(object):
         pickle.dump(self,f)
         f.close()
 
+    def saveMat(self,filename,dataname='rawesomeTrajectory'):
+        assert isinstance(filename,str), "filename must be a string"
+        ret = {}
+        for name in self.dvMap._xNames + \
+            self.dvMap._zNames + \
+            self.dvMap._uNames + \
+            self.outputMap._outputNames + \
+            self.quadratureMap._quadMap.keys():
+            (ts,ys) = self.getTimeSeries(name)
+            ts = numpy.array(ts)
+            ys = numpy.array(ys)
+            ret[name] = {'time':ts, 'value':ys}
+
+        print "saving trajectory as matlab file \"%s\"" % filename
+        scipy.io.savemat(filename, {dataname:ret})
+
+    def getTimeSeries(self,name):
+        # if it's a differential state
+        if name in self.dvMap._xNames:
+            ys = []
+            ts = []
+            for i in range(self.dvMap._nk):
+                for k in range(self.dvMap._nicp):
+                    for j in range(self.dvMap._deg+1):
+                        ys.append(self.dvMap.lookup(name,timestep=i,nicpIdx=k,degIdx=j))
+                        ts.append(self.tgrid[i,k,j])
+                    ys.append(numpy.nan*ys[-1])
+                    ts.append(ts[-1])
+            ys.append(self.dvMap.lookup(name,timestep=-1,nicpIdx=0,degIdx=0))
+            ts.append(self.tgrid[-1,0,0])
+            return (ts,ys)
+
+        # if it's an algebraic var
+        elif name in self.dvMap._zNames:
+            ys = []
+            ts = []
+            for i in range(self.dvMap._nk):
+                for k in range(self.dvMap._nicp):
+                    for j in range(1,self.dvMap._deg+1):
+                        ys.append(self.dvMap.lookup(name,timestep=i,nicpIdx=k,degIdx=j))
+                        ts.append(self.tgrid[i,k,j])
+                    ys.append(numpy.nan*ys[-1])
+                    ts.append(ts[-1])
+            return (ts,ys)
+
+        # if it's a control
+        elif name in self.dvMap._uNames:
+            ys = []
+            ts = []
+            for i in range(self.dvMap._nk):
+                y = self.dvMap.lookup(name,timestep=i)
+                t0 = self.tgrid[i,0,0]
+                t1 = self.tgrid[i+1,0,0]
+                ys.extend([y,y,numpy.nan*y])
+                ts.extend([t0,t1,t1])
+            return (ts,ys)
+
+        # if it's an output defined everywhere
+        elif name in self.outputMap._outputs0:
+            ys = []
+            ts = []
+            for i in range(self.dvMap._nk):
+                for k in range(self.dvMap._nicp):
+                    for j in range(self.dvMap._deg+1):
+                        ys.append(self.outputMap.lookup(name,timestep=i,nicpIdx=k,degIdx=j))
+                        ts.append(self.tgrid[i,k,j])
+                    ys.append(numpy.nan*ys[-1])
+                    ts.append(ts[-1])
+            return (ts, ys)
+
+        # if it's an output defined only on collocation points
+        elif name in self.outputMap._outputs:
+            ys = []
+            ts = []
+            for i in range(self.dvMap._nk):
+                for k in range(self.dvMap._nicp):
+                    for j in range(1,self.dvMap._deg+1):
+                        ys.append(self.outputMap.lookup(name,timestep=i,nicpIdx=k,degIdx=j))
+                        ts.append(self.tgrid[i,k,j])
+                    ys.append(numpy.nan*ys[-1])
+                    ts.append(ts[-1])
+            return (ts, ys)
+
+        # if it's a quadrature state
+        elif name in self.quadratureMap._quadMap:
+            ys = []
+            ts = []
+            for i in range(self.quadratureMap._nk):
+                for k in range(self.quadratureMap._nicp):
+                    for j in range(self.quadratureMap._deg+1):
+                        ys.append(self.quadratureMap.lookup(name,timestep=i,nicpIdx=k,degIdx=j))
+                        ts.append(self.tgrid[i,k,j])
+                    ys.append(numpy.nan*ys[-1])
+                    ts.append(ts[-1])
+            return (ts, ys)
+
+        # throw error on parameter
+        elif name in self.dvMap._pNames:
+            raise Exception("can't plot a parameter (\""+name+"\")")
+
+        # throw error on unrecognized
+        else:
+            raise Exception("unrecognized name \""+name+"\"")
+
+
 # thing which facilitates plotting
 class TrajectoryPlotter(Trajectory):
     """
@@ -84,7 +190,7 @@ class TrajectoryPlotter(Trajectory):
     """
     def subplot(self,names,title=None,style=None):
         assert isinstance(names,list)
-        
+
         fig = plt.figure()
         if title is None:
             if isinstance(names,str):
@@ -96,7 +202,7 @@ class TrajectoryPlotter(Trajectory):
                 else:
                     title = str(names)
         fig.canvas.set_window_title(str(title))
-                    
+
         plt.clf()
         n = len(names)
         if style is None:
@@ -119,7 +225,7 @@ class TrajectoryPlotter(Trajectory):
                     title = names[0]
                 else:
                     title = str(names)
-        
+
         fig.canvas.set_window_title(str(title))
         plt.clf()
         self._plot(names,title,style=style)
@@ -128,117 +234,17 @@ class TrajectoryPlotter(Trajectory):
         if isinstance(names,str):
             names = [names]
         assert isinstance(names,list)
-        
+
         legend = []
         for name in names:
             assert isinstance(name,str)
             legend.append(name)
-
-            # if it's a differential state
-            if name in self.dvMap._xNames:
-                ys = []
-                ts = []
-                for i in range(self.dvMap._nk):
-                    for k in range(self.dvMap._nicp):
-                        for j in range(self.dvMap._deg+1):
-                            ys.append(self.dvMap.lookup(name,timestep=i,nicpIdx=k,degIdx=j))
-                            ts.append(self.tgrid[i,k,j])
-                        ys.append(np.nan*ys[-1])
-                        ts.append(ts[-1])
-                ys.append(self.dvMap.lookup(name,timestep=-1,nicpIdx=0,degIdx=0))
-                ts.append(self.tgrid[-1,0,0])
-                if style is None:
-                    plt.plot(ts,ys)
-                else:
-                    plt.plot(ts,ys,style)
-                
-            # if it's an algebraic var
-            elif name in self.dvMap._zNames:
-                ys = []
-                ts = []
-                for i in range(self.dvMap._nk):
-                    for k in range(self.dvMap._nicp):
-                        for j in range(1,self.dvMap._deg+1):
-                            ys.append(self.dvMap.lookup(name,timestep=i,nicpIdx=k,degIdx=j))
-                            ts.append(self.tgrid[i,k,j])
-                        ys.append(np.nan*ys[-1])
-                        ts.append(ts[-1])
-                if style is None:
-                    plt.plot(ts,ys)
-                else:
-                    plt.plot(ts,ys,style)
-
-            # if it's a control
-            elif name in self.dvMap._uNames:
-                ys = []
-                ts = []
-                for i in range(self.dvMap._nk):
-                    y = self.dvMap.lookup(name,timestep=i)
-                    t0 = self.tgrid[i,0,0]
-                    t1 = self.tgrid[i+1,0,0]
-                    ys.extend([y,y,np.nan*y])
-                    ts.extend([t0,t1,t1])
-                if style is None:
-                    plt.plot(ts,ys)
-                else:
-                    plt.plot(ts,ys,style)
-
-            # if it's an output defined everywhere
-            elif name in self.outputMap._outputs0:
-                ys = []
-                ts = []
-                for i in range(self.dvMap._nk):
-                    for k in range(self.dvMap._nicp):
-                        for j in range(self.dvMap._deg+1):
-                            ys.append(self.outputMap.lookup(name,timestep=i,nicpIdx=k,degIdx=j))
-                            ts.append(self.tgrid[i,k,j])
-                        ys.append(np.nan*ys[-1])
-                        ts.append(ts[-1])
-                if style is None:
-                    plt.plot(ts,ys)
-                else:
-                    plt.plot(ts,ys,style)
-
-            # if it's an output defined only on collocation points
-            elif name in self.outputMap._outputs:
-                ys = []
-                ts = []
-                for i in range(self.dvMap._nk):
-                    for k in range(self.dvMap._nicp):
-                        for j in range(1,self.dvMap._deg+1):
-                            ys.append(self.outputMap.lookup(name,timestep=i,nicpIdx=k,degIdx=j))
-                            ts.append(self.tgrid[i,k,j])
-                        ys.append(np.nan*ys[-1])
-                        ts.append(ts[-1])
-                if style is None:
-                    plt.plot(ts,ys)
-                else:
-                    plt.plot(ts,ys,style)
-
-            # if it's a quadrature state
-            elif name in self.quadratureMap._quadMap:
-                ys = []
-                ts = []
-                for i in range(self.quadratureMap._nk):
-                    for k in range(self.quadratureMap._nicp):
-                        for j in range(self.quadratureMap._deg+1):
-                            ys.append(self.quadratureMap.lookup(name,timestep=i,nicpIdx=k,degIdx=j))
-                            ts.append(self.tgrid[i,k,j])
-                        ys.append(np.nan*ys[-1])
-                        ts.append(ts[-1])
-                if style is None:
-                    plt.plot(ts,ys)
-                else:
-                    plt.plot(ts,ys,style)
-
-            # throw error on parameter
-            elif name in self.dvMap._pNames:
-                raise ValueError("can't plot a parameter (\""+name+"\")")
-
-            # throw error on unrecognized
+            (ts,ys) = self.getTimeSeries(name)
+            if style is None:
+                plt.plot(ts,ys)
             else:
-                raise ValueError("unrecognized name \""+name+"\"")
-        
+                plt.plot(ts,ys,style)
+
         if title is not None:
             assert isinstance(title,str), "title must be a string"
             plt.title(title)
