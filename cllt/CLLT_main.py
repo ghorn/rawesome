be6440c40091f21292fc7f8d4c23a22ab13c5520
Greg Horn <gregmainland@gmail.com>
2013-05-31 18:19:37 +0200

    cllt: wrote diagnostic code and found the linspace(1,2n+/-1,n) bug, code now seems to be working

--- cllt/CLLT_main.py
+++ cllt/CLLT_main.py
@@ -1,18 +1,93 @@
 import LLT_solver
 from geometry import Geometry
 import numpy
+import casadi as C
+import pylab
 
-operAlphaDegLst = range(-5,15,1)
-operRates = [0,0,0]
+operAlphaDegLst = list(numpy.linspace(5,40,100))
 geomRoot = [1,0,0]
-geomTip = [1,5,0]
+geomTip = [0.4,5,0]
 aeroCLaRoot = numpy.array([0.0, 0.0, 2*numpy.pi, 0.0])
 aeroCLaTip  = numpy.array([0.0, 0.0, 2*numpy.pi, 0.0])
 #aeroCLaRoot = numpy.array([ -28.2136, 16.4140, 0.9568,-0.4000])
 #aeroCLaTip = numpy.array([ -28.2136, 16.4140, 0.9568, -0.4000])
 
 n = 30
-
 geom = Geometry(geomRoot, geomTip, aeroCLaRoot, aeroCLaTip, n)
+LLT_solver.LLT_solver(operAlphaDegLst, geom)
 
-LLT_solver.LLT_sovler(operAlphaDegLst, operRates, geom)
+
+# set up the LLT symbolics
+alpha = C.ssym('alpha')
+An = C.ssym('A',geom.n)
+(makeMeZero, alphaiLoc) = LLT_solver.setupImplicitFunction(alpha, An, geom)
+
+# make the solver
+f = C.SXFunction([An,alpha], [makeMeZero])
+f.init()
+
+solver = C.NLPImplicitSolver(f)
+solver.setOption('nlp_solver', C.IpoptSolver)
+#solver.setOption('nlp_solver_options',{'suppress_all_output':'yes','print_time':False})
+solver.init()
+
+fAlphai = C.SXFunction([An], [alphaiLoc])
+fAlphai.init()
+
+################ now lets make a run woo ##############
+operAlpha = numpy.radians(5)
+solver.setInput(operAlpha)
+guessAn = numpy.zeros(n)
+guessAn[0] = 0.014
+solver.setOutput(guessAn)
+solver.evaluate()
+An = numpy.squeeze(numpy.array(solver.output()))
+
+fAlphai.setInput(An)
+fAlphai.evaluate()
+alphaiLoc = numpy.squeeze(numpy.array(fAlphai.output()))
+alphaiLocDeg = numpy.degrees(alphaiLoc)
+
+# plot alpha
+pylab.plot(geom.yLoc, numpy.degrees(operAlpha)-alphaiLocDeg)
+pylab.legend(['alpha(deg)'])
+
+#for name in ['chordLoc','aIncGeometricLoc','thetaLoc']:
+#    pylab.figure()
+#    pylab.plot(geom.yLoc, getattr(geom,name))
+#    pylab.legend([name])
+
+
+# plot Cl
+pylab.figure()
+Cl = geom.clLoc(geom.alphaGeometric(operAlpha) - alphaiLoc)
+pylab.plot(geom.yLoc, Cl)
+Clellipse = Cl[-1]*numpy.sqrt(1 - geom.yLoc**2/25)
+pylab.plot(geom.yLoc, Clellipse/geom.chordLoc)
+pylab.legend(['Cl','Cl ellipse'])
+
+## plot Gamma
+#Gamma = numpy.dot(numpy.sin(numpy.outer(geom.thetaLoc, geom.sumN)), An)
+#pylab.figure()
+#pylab.plot(geom.thetaLoc, Gamma)
+#pylab.legend(['gamma'])
+#pylab.xlabel('theta')
+#
+#pylab.figure()
+#pylab.plot(geom.yLoc, Gamma)
+#pylab.legend(['gamma'])
+#pylab.xlabel('y')
+#
+#Gamma = numpy.sin(numpy.outer(geom.thetaLoc, geom.sumN*An))
+#pylab.figure()
+#pylab.plot(geom.yLoc, Gamma)
+#pylab.legend(['A'+str(k) for k in geom.sumN])
+#pylab.xlabel('y')
+
+
+# plot An
+pylab.figure()
+pylab.semilogy(solver.output(),'.')
+pylab.legend(['An'])
+
+pylab.show()
--- cllt/LLT_solver.py
+++ cllt/LLT_solver.py
@@ -3,96 +3,56 @@ import pylab
 import numpy
 import casadi as C
 
-def setupImplicitFunction(geom):
-    operAlpha      = C.ssym('alpha')
-    alphaGeometric = operAlpha + geom.aIncGeometricLoc
-
-    n = geom.n
+def setupImplicitFunction(operAlpha, An, geom):
 
     #setup function to obtain B and RHS for the Fourier series calc
-    def getBandRHS(iterAlphaiLoc):
-        alphaLoc = alphaGeometric - iterAlphaiLoc
-        clLoc = geom.clPolyLoc[0,:]*alphaLoc**3 + \
-                geom.clPolyLoc[1,:]*alphaLoc**2 + \
-                geom.clPolyLoc[2,:]*alphaLoc + \
-                geom.clPolyLoc[3,:]
+    def getBandRHS(alphaiLoc):
+        alphaLoc = geom.alphaGeometric(operAlpha) - alphaiLoc
+        clLoc = geom.clLoc(alphaLoc)
         RHS = clLoc*geom.chordLoc/(4.0*geom.bref)
         B = numpy.sin(numpy.outer(geom.thetaLoc, geom.sumN))
         return (B,RHS)
 
-    iterAn = C.ssym('iterAn',geom.n)
-    iterAlphaiLoc = []
-    for i in range(n):
-        iterAlphaiLoc.append(
-            C.inner_prod(geom.sumN*numpy.sin(geom.sumN*geom.thetaLoc[i])/numpy.sin(geom.thetaLoc[i]), iterAn))
-    iterAlphaiLoc = C.veccat(iterAlphaiLoc)
+    alphaiLoc = []
+    for i in range(geom.n):
+        alphaiLoc.append(
+            C.inner_prod(geom.sumN*numpy.sin(geom.sumN*geom.thetaLoc[i])/numpy.sin(geom.thetaLoc[i]), An))
+    alphaiLoc = C.veccat(alphaiLoc)
 
-    (B,RHS) = getBandRHS(iterAlphaiLoc)
-    makeMeZero = RHS - C.mul(B, iterAn)
-#    makeMeZero = C.solve(B,RHS) - iterAn
+    (B,RHS) = getBandRHS(alphaiLoc)
+    makeMeZero = RHS - C.mul(B, An)
 
-#    fIterAlphaiLoc = C.SXFunction([iterAn],[iterAlphaiLoc])
-#    fIterAlphaiLoc.init()
-#    fIterAlphaiLoc.setInput(iterAn)
-#    fIterAlphaiLoc.evaluate()
-#    iterAlphaiLoc = numpy.array(fIterAlphaiLoc.output())
-
-    print "setting up function"
-    f = C.SXFunction([iterAn,operAlpha], [makeMeZero])
-    print "initializing function"
-    f.init()
-
-    print "setting up solver"
-    i = C.NLPImplicitSolver(f)
-    i.setOption('nlp_solver', C.IpoptSolver)
-    print "initializing solver"
-    i.setOption('nlp_solver_options',{'suppress_all_output':'yes',
-                                      'print_time':False})
-    i.init()
-
-    print "done"
-    return i
+    return (makeMeZero, alphaiLoc)
 
 
-def LLT_sovler(operAlphaDegLst, operRates, geom):
-    #
-    #operAlpha is a 1x3 vector containing initial alpha, final alpha and alpha increment for a sweep.
-    #
-#    operAseqMin     = numpy.radians(operAseq[0])
-#    operAseqMax     = numpy.radians(operAseq[1])
-#    operAseqIncr    = numpy.radians(operAseq[2])
-    #
-    #now let's setup the range of alphas we are going to run this calc over
-#    operAlphaLst = []
-#    tempAlpha = operAseqMin
-#    while tempAlpha < (operAseqMax + operAseqIncr / 10):
-#        operAlphaLst.append(tempAlpha)
-#        tempAlpha = tempAlpha + operAseqIncr
+def LLT_solver(operAlphaDegLst, geom):
     operAlphaLst = [numpy.radians(x) for x in operAlphaDegLst]
-    #################Alpha hard set option
-    #operAlphaLst=[0]
-    ##########################################
-    #
-    #Before we start iterating, let's setup lists for storing lift and induced drag
-    #
     operCLLst   = []
     operCDiLst  = []
     #
-    #Next, lets choose the number of spanwise stations (and hence number of Fourier series terms) that we will divide each half wing into
-    #We will also pick a suitable convergence criterion for the non-linear iteration and a relaxation factor to aid convergence
-    #
-    #Finally, let's create n span stations at which LLT equations will be solved and define some fixed wing properties at each station
-    #
-    #
     #Now let's iterate over the Alphas and solve LLT equations to obtain lift for each one
     #
-    f = setupImplicitFunction(geom)
+    alpha = C.ssym('alpha')
+    An = C.ssym('A',geom.n)
+    (makeMeZero, alphaiLoc) = setupImplicitFunction(alpha, An, geom)
+    
+    # make the solver
+    f = C.SXFunction([An,alpha], [makeMeZero])
+    f.init()
+    
+    solver = C.NLPImplicitSolver(f)
+    solver.setOption('nlp_solver', C.IpoptSolver)
+    #solver.setOption('nlp_solver_options',{'suppress_all_output':'yes','print_time':False})
+    solver.init()
+
     for operAlpha in operAlphaLst:
         print 'Current Alpha = ', numpy.degrees(operAlpha)
-        f.setOutput(numpy.zeros(geom.n))
-        f.setInput(operAlpha)
-        f.evaluate()
-        iterAn = numpy.squeeze(numpy.array(f.output()))
+        guess = numpy.zeros(geom.n)
+        guess[0] = 0.01
+        solver.setOutput(guess,0)
+        solver.setInput(operAlpha)
+        solver.evaluate()
+        iterAn = numpy.squeeze(numpy.array(solver.output()))
 
         #Compute CL/CDi
         CL  = iterAn[0]*numpy.pi*geom.AR
@@ -115,12 +75,14 @@ def LLT_sovler(operAlphaDegLst, operRates, geom):
     pylab.xlabel('Alpha')
     pylab.ylabel('CL')
     pylab.legend(['llt CL','flat plate CL'])
-    pylab.show()
+
+    pylab.figure()
     pylab.plot(numpy.degrees(operAlphaLst),operCDiLst,'r',numpy.degrees(operAlphaLst),operCLLst[:]**2/(numpy.pi*geom.AR),'b')
     pylab.legend(['llt CDi','flat plate CDi'])
     pylab.xlabel('Alpha')
     pylab.ylabel('CDi')
-    pylab.show()
+
+    pylab.figure()
     pylab.plot(operCDiLst,operCLLst)
     pylab.xlabel('CDi')
     pylab.ylabel('CL')
