839e7f9a1f70afb301737698320e630587853e9b
Greg Horn <gregmainland@gmail.com>
2013-06-09 17:40:54 +0200

    fix sign error in gravity in crosswind, also don't double-count gravity in homotopy

--- rawe/models/crosswind.py
+++ rawe/models/crosswind.py
@@ -101,12 +101,12 @@ def setupModel(dae, conf):
     # if we are running a homotopy, add psudeo forces and moments as algebraic states
     if 'runHomotopy' in conf and conf['runHomotopy']:
         gamma_homotopy = dae.addP('gamma_homotopy')
-        f1 = f1 * gamma_homotopy + dae.addZ('f1_homotopy')          * (1 - gamma_homotopy)
-        f2 = f2 * gamma_homotopy + dae.addZ('f2_homotopy')          * (1 - gamma_homotopy)
-        f3 = f3 * gamma_homotopy + (dae.addZ('f3_homotopy') - g*m)  * (1 - gamma_homotopy)
-        t1 = t1 * gamma_homotopy + dae.addZ('t1_homotopy')          * (1 - gamma_homotopy)
-        t2 = t2 * gamma_homotopy + dae.addZ('t2_homotopy')          * (1 - gamma_homotopy)
-        t3 = t3 * gamma_homotopy + dae.addZ('t3_homotopy')          * (1 - gamma_homotopy)
+        f1 = f1 * gamma_homotopy + dae.addZ('f1_homotopy') * (1 - gamma_homotopy)
+        f2 = f2 * gamma_homotopy + dae.addZ('f2_homotopy') * (1 - gamma_homotopy)
+        f3 = f3 * gamma_homotopy + dae.addZ('f3_homotopy') * (1 - gamma_homotopy)
+        t1 = t1 * gamma_homotopy + dae.addZ('t1_homotopy') * (1 - gamma_homotopy)
+        t2 = t2 * gamma_homotopy + dae.addZ('t2_homotopy') * (1 - gamma_homotopy)
+        t3 = t3 * gamma_homotopy + dae.addZ('t3_homotopy') * (1 - gamma_homotopy)
 
     # mass matrix
     mm = C.SXMatrix(7, 7)
@@ -177,7 +177,7 @@ def setupModel(dae, conf):
     rhs = C.veccat(
           [ f1 + ddelta*m*(dy + ddelta*rA + ddelta*x) + ddelta*dy*m
           , f2 - ddelta*m*(dx - ddelta*y) - ddelta*dx*m
-          , f3 - g*m
+          , f3 + g*m
           , t1 - w2*(j3*w3 + j31*w1) + j2*w2*w3
           , t2 + w1*(j3*w3 + j31*w1) - w3*(j1*w1 + j31*w3)
           , t3 + w2*(j1*w1 + j31*w3) - j2*w1*w2
