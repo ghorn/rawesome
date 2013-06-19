122c4c2ac6c1b0b02e75d60b3d0b7b5dbb31367d
Greg Horn <gregmainland@gmail.com>
2013-06-19 15:18:01 +0200

    clean up crosswind model

--- rawe/models/crosswind.py
+++ rawe/models/crosswind.py
@@ -19,7 +19,36 @@ import casadi as C
 from rawe.dae import Dae
 from aero import aeroForcesTorques
 
-def setupModel(dae, conf):
+def get_wind(dae, conf):
+    '''
+    return the wind at current altitude based on configuration
+    '''
+    if conf['wind_model']['name'] == 'wind_shear':
+        # use a logarithmic wind shear model
+        # wind(z) = w0 * log((z+zt)/zt) / log(z0/zt)
+        # where w0 is wind at z0 altitude
+        # zt is surface roughness characteristic length
+        z = dae['z']
+        z0 = conf['wind_model']['z0']
+        zt_roughness = conf['wind_model']['zt_roughness']
+        return dae['w0']*C.log((-z+zt_roughness)/zt_roughness) / \
+                         C.log(z0/zt_roughness)
+    elif conf['wind_model']['name'] == 'constant':
+        # constant wind
+        return dae['w0']
+
+def skew_mat(xyz):
+    '''
+    return the skew symmetrix matrix of a 3-vector
+    '''
+    x = xyz[0]
+    y = xyz[1]
+    z = xyz[2]
+    return C.vertcat([C.horzcat([ 0, -z,  y]),
+                      C.horzcat([ z,  0, -x]),
+                      C.horzcat([-y,  x,  0])])
+
+def compute_mass_matrix(dae, conf, f1, f2, f3, t1, t2, t3):
     '''
     take the dae that has x/z/u/p added to it already and return
     the states added to it and return mass matrix and rhs of the dae residual
@@ -57,103 +86,64 @@ def setupModel(dae, conf):
     r = dae['r']
     dr = dae['dr']
     ddr = dae['ddr']
-
-    # wind model
-    def getWind():
-        if conf['wind_model']['name'] == 'wind_shear':
-            # use a logarithmic wind shear model
-            # wind(z) = w0 * log((z+zt)/zt) / log(z0/zt)
-            # where w0 is wind at z0 altitude
-            # zt is surface roughness characteristic length
-            z0 = conf['wind_model']['z0']
-            zt_roughness = conf['wind_model']['zt_roughness']
-            return dae['w0']*C.log((-z+zt_roughness)/zt_roughness)/C.log(z0/zt_roughness)
-        elif conf['wind_model']['name'] == 'constant':
-            # constant wind
-            return dae['w0']
-
-    dae['wind_at_altitude'] = getWind()
-    # wind velocity in NED
-    dae['v_wn_n'] = C.veccat([dae['wind_at_altitude'], 0, 0])
-    # body velocity in NED
-    dae['v_bn_n'] = C.veccat( [ dx , dy, dz ] )
-    # body velocity w.r.t. wind in NED
-    v_bw_n =  dae['v_bn_n'] - dae['v_wn_n']
-    # body velocity w.r.t. wind in body frame
-    v_bw_b = C.mul( dae['R_n2b'], v_bw_n )
-
-    # compute aerodynamic forces and moments
-    (f1, f2, f3, t1, t2, t3) = aeroForcesTorques(dae, conf, v_bw_n, v_bw_b,
-                                                 dae['w_bn_b'],
-                                                 (dae['e21'], dae['e22'], dae['e23'])
-                                                 )
-    # if we are running a homotopy, add psudeo forces and moments as algebraic states
-    if 'runHomotopy' in conf and conf['runHomotopy']:
-        gamma_homotopy = dae.addP('gamma_homotopy')
-        f1 = f1 * gamma_homotopy + dae.addZ('f1_homotopy') * (1 - gamma_homotopy)
-        f2 = f2 * gamma_homotopy + dae.addZ('f2_homotopy') * (1 - gamma_homotopy)
-        f3 = f3 * gamma_homotopy + dae.addZ('f3_homotopy') * (1 - gamma_homotopy)
-        t1 = t1 * gamma_homotopy + dae.addZ('t1_homotopy') * (1 - gamma_homotopy)
-        t2 = t2 * gamma_homotopy + dae.addZ('t2_homotopy') * (1 - gamma_homotopy)
-        t3 = t3 * gamma_homotopy + dae.addZ('t3_homotopy') * (1 - gamma_homotopy)
-
+    
     # mass matrix
     mm = C.SXMatrix(7, 7)
-    mm[0,0] = m
-    mm[0,1] = 0
-    mm[0,2] = 0
-    mm[0,3] = 0
-    mm[0,4] = 0
-    mm[0,5] = 0
-    mm[0,6] = x + zt*e31
+    mm[0, 0] = m
+    mm[0, 1] = 0
+    mm[0, 2] = 0
+    mm[0, 3] = 0
+    mm[0, 4] = 0
+    mm[0, 5] = 0
+    mm[0, 6] = x + zt*e31
 
-    mm[1,0] = 0
-    mm[1,1] = m
-    mm[1,2] = 0
-    mm[1,3] = 0
-    mm[1,4] = 0
-    mm[1,5] = 0
-    mm[1,6] = y + zt*e32
+    mm[1, 0] = 0
+    mm[1, 1] = m
+    mm[1, 2] = 0
+    mm[1, 3] = 0
+    mm[1, 4] = 0
+    mm[1, 5] = 0
+    mm[1, 6] = y + zt*e32
 
-    mm[2,0] = 0
-    mm[2,1] = 0
-    mm[2,2] = m
-    mm[2,3] = 0
-    mm[2,4] = 0
-    mm[2,5] = 0
-    mm[2,6] = z + zt*e33
+    mm[2, 0] = 0
+    mm[2, 1] = 0
+    mm[2, 2] = m
+    mm[2, 3] = 0
+    mm[2, 4] = 0
+    mm[2, 5] = 0
+    mm[2, 6] = z + zt*e33
 
-    mm[3,0] = 0
-    mm[3,1] = 0
-    mm[3,2] = 0
-    mm[3,3] = j1
-    mm[3,4] = 0
-    mm[3,5] = j31
-    mm[3,6] = -zt*(e21*x + e22*y + e23*z + zt*e21*e31 + zt*e22*e32 + zt*e23*e33)
+    mm[3, 0] = 0
+    mm[3, 1] = 0
+    mm[3, 2] = 0
+    mm[3, 3] = j1
+    mm[3, 4] = 0
+    mm[3, 5] = j31
+    mm[3, 6] = -zt*(e21*x + e22*y + e23*z + zt*e21*e31 + zt*e22*e32 + zt*e23*e33)
 
-    mm[4,0] = 0
-    mm[4,1] = 0
-    mm[4,2] = 0
-    mm[4,3] = 0
-    mm[4,4] = j2
-    mm[4,5] = 0
-    mm[4,6] = zt*(e11*x + e12*y + e13*z + zt*e11*e31 + zt*e12*e32 + zt*e13*e33)
+    mm[4, 0] = 0
+    mm[4, 1] = 0
+    mm[4, 2] = 0
+    mm[4, 3] = 0
+    mm[4, 4] = j2
+    mm[4, 5] = 0
+    mm[4, 6] = zt*(e11*x + e12*y + e13*z + zt*e11*e31 + zt*e12*e32 + zt*e13*e33)
 
-    mm[5,0] = 0
-    mm[5,1] = 0
-    mm[5,2] = 0
-    mm[5,3] = j31
-    mm[5,4] = 0
-    mm[5,5] = j3
-    mm[5,6] = 0
+    mm[5, 0] = 0
+    mm[5, 1] = 0
+    mm[5, 2] = 0
+    mm[5, 3] = j31
+    mm[5, 4] = 0
+    mm[5, 5] = j3
+    mm[5, 6] = 0
 
-    mm[6,0] = x + zt*e31
-    mm[6,1] = y + zt*e32
-    mm[6,2] = z + zt*e33
-    mm[6,3] = -zt*(e21*x + e22*y + e23*z + zt*e21*e31 + zt*e22*e32 + zt*e23*e33)
-    mm[6,4] = zt*(e11*x + e12*y + e13*z + zt*e11*e31 + zt*e12*e32 + zt*e13*e33)
-    mm[6,5] = 0
-    mm[6,6] = 0
+    mm[6, 0] = x + zt*e31
+    mm[6, 1] = y + zt*e32
+    mm[6, 2] = z + zt*e33
+    mm[6, 3] = -zt*(e21*x + e22*y + e23*z + zt*e21*e31 + zt*e22*e32 + zt*e23*e33)
+    mm[6, 4] = zt*(e11*x + e12*y + e13*z + zt*e11*e31 + zt*e12*e32 + zt*e13*e33)
+    mm[6, 5] = 0
+    mm[6, 6] = 0
 
     # right hand side
     w1  =  dae['w_bn_b_x']
@@ -161,66 +151,31 @@ def setupModel(dae, conf):
     w3  =  dae['w_bn_b_z']
 
     zt2 = zt*zt
-    ddelta = 0
-    rA = 0
     rhs = C.veccat(
-          [ f1 + ddelta*m*(dy + ddelta*rA + ddelta*x) + ddelta*dy*m
-          , f2 - ddelta*m*(dx - ddelta*y) - ddelta*dx*m
+          [ f1
+          , f2
           , f3 + g*m
           , t1 - w2*(j3*w3 + j31*w1) + j2*w2*w3
           , t2 + w1*(j3*w3 + j31*w1) - w3*(j1*w1 + j31*w3)
           , t3 + w2*(j1*w1 + j31*w3) - j2*w1*w2
-          , ddr*r-(zt*w1*(e11*x+e12*y+e13*z+zt*e11*e31+zt*e12*e32+zt*e13*e33)+zt*w2*(e21*x+e22*y+e23*z+zt*e21*e31+zt*e22*e32+zt*e23*e33))*(w3-ddelta*e33)-dx*(dx-zt*e21*(w1-ddelta*e13)+zt*e11*(w2-ddelta*e23))-dy*(dy-zt*e22*(w1-ddelta*e13)+zt*e12*(w2-ddelta*e23))-dz*(dz-zt*e23*(w1-ddelta*e13)+zt*e13*(w2-ddelta*e23))+dr*dr+(w1-ddelta*e13)*(e21*(zt*dx-zt2*e21*(w1-ddelta*e13)+zt2*e11*(w2-ddelta*e23))+e22*(zt*dy-zt2*e22*(w1-ddelta*e13)+zt2*e12*(w2-ddelta*e23))+zt*e23*(dz+zt*e13*w2-zt*e23*w1)+zt*e33*(w1*z+zt*e33*w1+ddelta*e11*x+ddelta*e12*y+zt*ddelta*e11*e31+zt*ddelta*e12*e32)+zt*e31*(x+zt*e31)*(w1-ddelta*e13)+zt*e32*(y+zt*e32)*(w1-ddelta*e13))-(w2-ddelta*e23)*(e11*(zt*dx-zt2*e21*(w1-ddelta*e13)+zt2*e11*(w2-ddelta*e23))+e12*(zt*dy-zt2*e22*(w1-ddelta*e13)+zt2*e12*(w2-ddelta*e23))+zt*e13*(dz+zt*e13*w2-zt*e23*w1)-zt*e33*(w2*z+zt*e33*w2+ddelta*e21*x+ddelta*e22*y+zt*ddelta*e21*e31+zt*ddelta*e22*e32)-zt*e31*(x+zt*e31)*(w2-ddelta*e23)-zt*e32*(y+zt*e32)*(w2-ddelta*e23))
+          , ddr*r-(zt*w1*(e11*x+e12*y+e13*z+zt*e11*e31+zt*e12*e32+zt*e13*e33)+zt*w2*(e21*x+e22*y+e23*z+zt*e21*e31+zt*e22*e32+zt*e23*e33))*(w3)-dx*(dx-zt*e21*(w1)+zt*e11*(w2))-dy*(dy-zt*e22*(w1)+zt*e12*(w2))-dz*(dz-zt*e23*(w1)+zt*e13*(w2))+dr*dr+(w1)*(e21*(zt*dx-zt2*e21*(w1)+zt2*e11*(w2))+e22*(zt*dy-zt2*e22*(w1)+zt2*e12*(w2))+zt*e23*(dz+zt*e13*w2-zt*e23*w1)+zt*e33*(w1*z+zt*e33*w1)+zt*e31*(x+zt*e31)*(w1)+zt*e32*(y+zt*e32)*(w1))-(w2)*(e11*(zt*dx-zt2*e21*(w1)+zt2*e11*(w2))+e12*(zt*dy-zt2*e22*(w1)+zt2*e12*(w2))+zt*e13*(dz+zt*e13*w2-zt*e23*w1)-zt*e33*(w2*z+zt*e33*w2)-zt*e31*(x+zt*e31)*(w2)-zt*e32*(y+zt*e32)*(w2))
           ] )
 
-    dRexp = C.SXMatrix(3,3)
+    dae['c'] = (x + zt*e31)**2/2 + (y + zt*e32)**2/2 + (z + zt*e33)**2/2 - r**2/2
 
-    dRexp[0,0] = e21*(w3 - ddelta*e33) - e31*(w2 - ddelta*e23)
-    dRexp[0,1] = e31*(w1 - ddelta*e13) - e11*(w3 - ddelta*e33)
-    dRexp[0,2] = e11*(w2 - ddelta*e23) - e21*(w1 - ddelta*e13)
-
-    dRexp[1,0] = e22*(w3 - ddelta*e33) - e32*(w2 - ddelta*e23)
-    dRexp[1,1] = e32*(w1 - ddelta*e13) - e12*(w3 - ddelta*e33)
-    dRexp[1,2] = e12*(w2 - ddelta*e23) - e22*(w1 - ddelta*e13)
-
-    dRexp[2,0] = e23*w3 - e33*w2
-    dRexp[2,1] = e33*w1 - e13*w3
-    dRexp[2,2] = e13*w2 - e23*w1
-
-    c =(x + zt*e31)**2/2 + (y + zt*e32)**2/2 + (z + zt*e33)**2/2 - r**2/2
-
-    cdot =dx*(x + zt*e31) + dy*(y + zt*e32) + dz*(z + zt*e33) + zt*(w2 - ddelta*e23)*(e11*x + e12*y + e13*z + zt*e11*e31 + zt*e12*e32 + zt*e13*e33) - zt*(w1 - ddelta*e13)*(e21*x + e22*y + e23*z + zt*e21*e31 + zt*e22*e32 + zt*e23*e33) - r*dr
+    dae['cdot'] = dx*(x + zt*e31) + dy*(y + zt*e32) + dz*(z + zt*e33) + zt*(w2)*(e11*x + e12*y + e13*z + zt*e11*e31 + zt*e12*e32 + zt*e13*e33) - zt*(w1)*(e21*x + e22*y + e23*z + zt*e21*e31 + zt*e22*e32 + zt*e23*e33) - r*dr
 
     ddx = dae.ddt('dx')
     ddy = dae.ddt('dy')
     ddz = dae.ddt('dz')
     dw1 = dae.ddt('w_bn_b_x')
     dw2 = dae.ddt('w_bn_b_y')
-#    ddx = dae['ddx']
-#    ddy = dae['ddy']
-#    ddz = dae['ddz']
-#    dw1 = dae['dw1']
-#    dw2 = dae['dw2']
+    ddelta = 0
     dddelta = 0
 
-    cddot = -(w1-ddelta*e13)*(zt*e23*(dz+zt*e13*w2-zt*e23*w1)+zt*e33*(w1*z+zt*e33*w1+ddelta*e11*x+ddelta*e12*y+zt*ddelta*e11*e31+zt*ddelta*e12*e32)+zt*e21*(dx+zt*e11*w2-zt*e21*w1-zt*ddelta*e11*e23+zt*ddelta*e13*e21)+zt*e22*(dy+zt*e12*w2-zt*e22*w1-zt*ddelta*e12*e23+zt*ddelta*e13*e22)+zt*e31*(x+zt*e31)*(w1-ddelta*e13)+zt*e32*(y+zt*e32)*(w1-ddelta*e13))+(w2-ddelta*e23)*(zt*e13*(dz+zt*e13*w2-zt*e23*w1)-zt*e33*(w2*z+zt*e33*w2+ddelta*e21*x+ddelta*e22*y+zt*ddelta*e21*e31+zt*ddelta*e22*e32)+zt*e11*(dx+zt*e11*w2-zt*e21*w1-zt*ddelta*e11*e23+zt*ddelta*e13*e21)+zt*e12*(dy+zt*e12*w2-zt*e22*w1-zt*ddelta*e12*e23+zt*ddelta*e13*e22)-zt*e31*(x+zt*e31)*(w2-ddelta*e23)-zt*e32*(y+zt*e32)*(w2-ddelta*e23))-ddr*r+(zt*w1*(e11*x+e12*y+e13*z+zt*e11*e31+zt*e12*e32+zt*e13*e33)+zt*w2*(e21*x+e22*y+e23*z+zt*e21*e31+zt*e22*e32+zt*e23*e33))*(w3-ddelta*e33)+dx*(dx+zt*e11*w2-zt*e21*w1-zt*ddelta*e11*e23+zt*ddelta*e13*e21)+dy*(dy+zt*e12*w2-zt*e22*w1-zt*ddelta*e12*e23+zt*ddelta*e13*e22)+dz*(dz+zt*e13*w2-zt*e23*w1)+ddx*(x+zt*e31)+ddy*(y+zt*e32)+ddz*(z+zt*e33)-dr*dr+zt*(dw2-dddelta*e23)*(e11*x+e12*y+e13*z+zt*e11*e31+zt*e12*e32+zt*e13*e33)-zt*(dw1-dddelta*e13)*(e21*x+e22*y+e23*z+zt*e21*e31+zt*e22*e32+zt*e23*e33)-zt*dddelta*(e11*e23*x-e13*e21*x+e12*e23*y-e13*e22*y+zt*e11*e23*e31-zt*e13*e21*e31+zt*e12*e23*e32-zt*e13*e22*e32)
+    dae['cddot'] = -(w1-ddelta*e13)*(zt*e23*(dz+zt*e13*w2-zt*e23*w1)+zt*e33*(w1*z+zt*e33*w1+ddelta*e11*x+ddelta*e12*y+zt*ddelta*e11*e31+zt*ddelta*e12*e32)+zt*e21*(dx+zt*e11*w2-zt*e21*w1-zt*ddelta*e11*e23+zt*ddelta*e13*e21)+zt*e22*(dy+zt*e12*w2-zt*e22*w1-zt*ddelta*e12*e23+zt*ddelta*e13*e22)+zt*e31*(x+zt*e31)*(w1-ddelta*e13)+zt*e32*(y+zt*e32)*(w1-ddelta*e13))+(w2-ddelta*e23)*(zt*e13*(dz+zt*e13*w2-zt*e23*w1)-zt*e33*(w2*z+zt*e33*w2+ddelta*e21*x+ddelta*e22*y+zt*ddelta*e21*e31+zt*ddelta*e22*e32)+zt*e11*(dx+zt*e11*w2-zt*e21*w1-zt*ddelta*e11*e23+zt*ddelta*e13*e21)+zt*e12*(dy+zt*e12*w2-zt*e22*w1-zt*ddelta*e12*e23+zt*ddelta*e13*e22)-zt*e31*(x+zt*e31)*(w2-ddelta*e23)-zt*e32*(y+zt*e32)*(w2-ddelta*e23))-ddr*r+(zt*w1*(e11*x+e12*y+e13*z+zt*e11*e31+zt*e12*e32+zt*e13*e33)+zt*w2*(e21*x+e22*y+e23*z+zt*e21*e31+zt*e22*e32+zt*e23*e33))*(w3-ddelta*e33)+dx*(dx+zt*e11*w2-zt*e21*w1-zt*ddelta*e11*e23+zt*ddelta*e13*e21)+dy*(dy+zt*e12*w2-zt*e22*w1-zt*ddelta*e12*e23+zt*ddelta*e13*e22)+dz*(dz+zt*e13*w2-zt*e23*w1)+ddx*(x+zt*e31)+ddy*(y+zt*e32)+ddz*(z+zt*e33)-dr*dr+zt*(dw2-dddelta*e23)*(e11*x+e12*y+e13*z+zt*e11*e31+zt*e12*e32+zt*e13*e33)-zt*(dw1-dddelta*e13)*(e21*x+e22*y+e23*z+zt*e21*e31+zt*e22*e32+zt*e23*e33)-zt*dddelta*(e11*e23*x-e13*e21*x+e12*e23*y-e13*e22*y+zt*e11*e23*e31-zt*e13*e21*e31+zt*e12*e23*e32-zt*e13*e22*e32)
 
-#    cddot = (zt*w1*(e11*x + e12*y + e13*z + zt*e11*e31 + zt*e12*e32 + zt*e13*e33) + zt*w2*(e21*x + e22*y + e23*z + zt*e21*e31 + zt*e22*e32 + zt*e23*e33))*(w3 - ddelta*e33) + dx*(dx + zt*e11*w2 - zt*e21*w1 - zt*ddelta*e11*e23 + zt*ddelta*e13*e21) + dy*(dy + zt*e12*w2 - zt*e22*w1 - zt*ddelta*e12*e23 + zt*ddelta*e13*e22) + dz*(dz + zt*e13*w2 - zt*e23*w1) + ddx*(x + zt*e31) + ddy*(y + zt*e32) + ddz*(z + zt*e33) - (w1 - ddelta*e13)*(e21*(zt*dx - zt**2*e21*(w1 - ddelta*e13) + zt**2*e11*(w2 - ddelta*e23)) + e22*(zt*dy - zt**2*e22*(w1 - ddelta*e13) + zt**2*e12*(w2 - ddelta*e23)) + zt*e33*(z*w1 + ddelta*e11*x + ddelta*e12*y + zt*e33*w1 + zt*ddelta*e11*e31 + zt*ddelta*e12*e32) + zt*e23*(dz + zt*e13*w2 - zt*e23*w1) + zt*e31*(w1 - ddelta*e13)*(x + zt*e31) + zt*e32*(w1 - ddelta*e13)*(y + zt*e32)) + (w2 - ddelta*e23)*(e11*(zt*dx - zt**2*e21*(w1 - ddelta*e13) + zt**2*e11*(w2 - ddelta*e23)) + e12*(zt*dy - zt**2*e22*(w1 - ddelta*e13) + zt**2*e12*(w2 - ddelta*e23)) - zt*e33*(z*w2 + ddelta*e21*x + ddelta*e22*y + zt*e33*w2 + zt*ddelta*e21*e31 + zt*ddelta*e22*e32) + zt*e13*(dz + zt*e13*w2 - zt*e23*w1) - zt*e31*(w2 - ddelta*e23)*(x + zt*e31) - zt*e32*(w2 - ddelta*e23)*(y + zt*e32)) + zt*(dw2 - dddelta*e23)*(e11*x + e12*y + e13*z + zt*e11*e31 + zt*e12*e32 + zt*e13*e33) - zt*(dw1 - dddelta*e13)*(e21*x + e22*y + e23*z + zt*e21*e31 + zt*e22*e32 + zt*e23*e33) - zt*dddelta*(e11*e23*x - e13*e21*x + e12*e23*y - e13*e22*y + zt*e11*e23*e31 - zt*e13*e21*e31 + zt*e12*e23*e32 - zt*e13*e22*e32)
-#      where
-#        dw1 = dw @> 0
-#        dw2 = dw @> 1
-#        {-
-#        dw3 = dw @> 2
-#        -}
-#        ddx = ddX @> 0
-#        ddy = ddX @> 1
-#        ddz = ddX @> 2
-#        dddelta = dddelta' @> 0
-
-    dae['c'] =  c
-    dae['cdot'] = cdot
-    dae['cddot'] = cddot
-    return (mm, rhs, dRexp)
+    return (mm, rhs)
 
 def crosswindModel(conf):
     '''
@@ -230,37 +185,20 @@ def crosswindModel(conf):
     dae = Dae()
 
     # add some differential states/algebraic vars/controls/params
-    dae.addZ("nu")
-    dae.addX( [ "x"
-              , "y"
-              , "z"
-              , "e11"
-              , "e12"
-              , "e13"
-              , "e21"
-              , "e22"
-              , "e23"
-              , "e31"
-              , "e32"
-              , "e33"
-              , "dx"
-              , "dy"
-              , "dz"
-              , "w_bn_b_x"
-              , "w_bn_b_y"
-              , "w_bn_b_z"
-              , "r"
-              , "dr"
-              , 'ddr'
-              , "aileron"
-              , "elevator"
-              , "rudder"
-              , "flaps"
-              ] )
-    dae.addU( [ "daileron"
-              , "delevator"
-              , "drudder"
-              , "dflaps"
+    dae.addZ('nu')
+    dae.addX( ['x', 'y', 'z',
+               'e11', 'e12', 'e13',
+               'e21', 'e22', 'e23',
+               'e31', 'e32', 'e33',
+               'dx', 'dy', 'dz',
+               'w_bn_b_x', 'w_bn_b_y', 'w_bn_b_z',
+               'r', 'dr', 'ddr',
+               'aileron', 'elevator', 'rudder', 'flaps'
+           ])
+    dae.addU( [ 'daileron'
+              , 'delevator'
+              , 'drudder'
+              , 'dflaps'
               , 'dddr'
               ] )
     dae.addP( ['w0'] )
@@ -296,21 +234,56 @@ def crosswindModel(conf):
                                      0.0665919381751*dae['torque']*dae['torque'] + \
                                      0.1078628659825*dae['rpm']*dae['torque']
 
-    dae['R_n2b'] = C.vertcat([C.horzcat([dae['e11'],dae['e12'],dae['e13']]),
-                              C.horzcat([dae['e21'],dae['e22'],dae['e23']]),
-                              C.horzcat([dae['e31'],dae['e32'],dae['e33']])])
+    dae['R_n2b'] = C.vertcat([C.horzcat([dae['e11'], dae['e12'], dae['e13']]),
+                              C.horzcat([dae['e21'], dae['e22'], dae['e23']]),
+                              C.horzcat([dae['e31'], dae['e32'], dae['e33']])])
+
+    # local wind
+    dae['wind_at_altitude'] = get_wind(dae, conf)
+
+    # wind velocity in NED
+    dae['v_wn_n'] = C.veccat([dae['wind_at_altitude'], 0, 0])
+
+    # body velocity in NED
+    dae['v_bn_n'] = C.veccat( [ dae['dx'] , dae['dy'], dae['dz'] ] )
+
+    # body velocity w.r.t. wind in NED
+    v_bw_n =  dae['v_bn_n'] - dae['v_wn_n']
+
+    # body velocity w.r.t. wind in body frame
+    v_bw_b = C.mul( dae['R_n2b'], v_bw_n )
+
+    # compute aerodynamic forces and moments
+    (f1, f2, f3, t1, t2, t3) = \
+        aeroForcesTorques(dae, conf, v_bw_n, v_bw_b,
+                          dae['w_bn_b'],
+                          (dae['e21'], dae['e22'], dae['e23']))
+
+    # if we are running a homotopy, 
+    # add psudeo forces and moments as algebraic states
+    if 'runHomotopy' in conf and conf['runHomotopy']:
+        gamma_homotopy = dae.addP('gamma_homotopy')
+        f1 = f1*gamma_homotopy + dae.addZ('f1_homotopy')*(1 - gamma_homotopy)
+        f2 = f2*gamma_homotopy + dae.addZ('f2_homotopy')*(1 - gamma_homotopy)
+        f3 = f3*gamma_homotopy + dae.addZ('f3_homotopy')*(1 - gamma_homotopy)
+        t1 = t1*gamma_homotopy + dae.addZ('t1_homotopy')*(1 - gamma_homotopy)
+        t2 = t2*gamma_homotopy + dae.addZ('t2_homotopy')*(1 - gamma_homotopy)
+        t3 = t3*gamma_homotopy + dae.addZ('t3_homotopy')*(1 - gamma_homotopy)
+
+    # derivative of dcm
+    dRexp = C.mul(skew_mat(dae['w_bn_b']).T, dae['R_n2b'])
+    ddt_R_n2b = C.vertcat(\
+        [C.horzcat([dae.ddt(name) for name in ['e11', 'e12', 'e13']]),
+         C.horzcat([dae.ddt(name) for name in ['e21', 'e22', 'e23']]),
+         C.horzcat([dae.ddt(name) for name in ['e31', 'e32', 'e33']])])
 
     # get mass matrix, rhs
-    (massMatrix, rhs, dRexp) = setupModel(dae, conf)
+    (mass_matrix, rhs) = compute_mass_matrix(dae, conf, f1, f2, f3, t1, t2, t3)
 
     # set up the residual
     ode = C.veccat([
         C.veccat([dae.ddt(name) for name in ['x','y','z']]) - C.veccat([dae['dx'],dae['dy'],dae['dz']]),
-        C.veccat([dae.ddt(name) for name in ["e11","e12","e13",
-                                             "e21","e22","e23",
-                                             "e31","e32","e33"]]) - dRexp.trans().reshape([9,1]),
-#        C.veccat([dae.ddt(name) for name in ['dx','dy','dz']]) - C.veccat([dae['ddx'],dae['ddy'],dae['ddz']]),
-#        C.veccat([dae.ddt(name) for name in ['w1','w2','w3']]) - C.veccat([dae['dw1'],dae['dw2'],dae['dw3']]),
+        C.veccat([ddt_R_n2b - dRexp]),
         dae.ddt('r') - dae['dr'],
         dae.ddt('dr') - dae['ddr'],
         dae.ddt('ddr') - dae['dddr'],
@@ -331,7 +304,8 @@ def crosswindModel(conf):
 
     # line angle
     dae['cos_line_angle'] = \
-      -(dae['e31']*dae['x'] + dae['e32']*dae['y'] + dae['e33']*dae['z']) / C.sqrt(dae['x']**2 + dae['y']**2 + dae['z']**2)
+      -(dae['e31']*dae['x'] + dae['e32']*dae['y'] + dae['e33']*dae['z']) / \
+       C.sqrt(dae['x']**2 + dae['y']**2 + dae['z']**2)
     dae['line_angle_deg'] = C.arccos(dae['cos_line_angle'])*180.0/C.pi
 
     # add local loyd's limit
@@ -349,9 +323,9 @@ def crosswindModel(conf):
         dae['neg_mechanical_winch_power'] = -dae['mechanical_winch_power']
     addLoydsLimit()
 
-    psuedoZVec = C.veccat([dae.ddt(name) for name in \
-                    ['dx','dy','dz','w_bn_b_x','w_bn_b_y','w_bn_b_z']]+[dae['nu']])
-    alg = C.mul(massMatrix, psuedoZVec) - rhs
+    psuedo_zvec = C.veccat([dae.ddt(name) for name in \
+        ['dx','dy','dz','w_bn_b_x','w_bn_b_y','w_bn_b_z']]+[dae['nu']])
+    alg = C.mul(mass_matrix, psuedo_zvec) - rhs
     dae.setResidual( [ode, alg] )
 
     return dae
