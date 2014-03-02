b3ed820266ec70903b79952a5539c972d75215e1
Milan Vukov <mvukov@gmail.com>
2014-03-02 19:25:09 +0100

    Subprocess tee -- do not plot all crap by default, only when it fails.

--- rawe/utils/subprocess_tee.py
+++ rawe/utils/subprocess_tee.py
@@ -19,8 +19,8 @@ import subprocess
 import sys
 import select
 
-def call(args, cwd='.'):
-    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
+def call(args, cwd = '.'):
+    p = subprocess.Popen(args, stdout = subprocess.PIPE, stderr = subprocess.PIPE, cwd = cwd)
 
     msgs = []
 
@@ -31,11 +31,11 @@ def call(args, cwd='.'):
         for fd in ret[0]:
             if fd == p.stdout.fileno():
                 read = p.stdout.readline()
-                sys.stdout.write(read)
+#                 sys.stdout.write(read)
                 msgs.append(read)
             if fd == p.stderr.fileno():
                 read = p.stderr.readline()
-                sys.stderr.write(read)
+#                 sys.stderr.write(read)
                 msgs.append(read)
 
         ret = p.poll()
