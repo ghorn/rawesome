36b26035a07679f59346d7a3ca0d6d2c0a17b00f
Milan Vukov <mvukov@gmail.com>
2014-03-06 17:03:56 +0100

    Extended the SS computation interface

--- rawekite/carouselSteadyState.py
+++ rawekite/carouselSteadyState.py
@@ -29,7 +29,8 @@ def makeOrthonormal(g, R):
     g.add(rhon[2], '==', 0, tag = ('R1[0]: ( e1^T X e2 - e3 )[1] == 0', None))
     g.add(rhon[1], '==', 0, tag = ('R1[0]: ( e1^T X e2 - e3 )[2] == 0', None))
 
-def getSteadyState(dae, conf, omega0, r0, ref_dict = {}):
+def getSteadyState(dae, conf, omega0, r0, ref_dict = {},
+                   bounds = None, dotBounds = None, guess = None, dotGuess = None, verbose = True):
     # make steady state model
     g = Constraints()
     g.add(dae.getResidual(), '==', 0, tag = ('dae residual', None))
@@ -58,65 +59,69 @@ def getSteadyState(dae, conf, omega0, r0, ref_dict = {}):
     ffcn.init()
     gfcn.init()
 
-    guess = {'x':r0, 'y':0, 'z':0,
-             'r':r0, 'dr':0,
-             'e11':0, 'e12':-1, 'e13':0,
-             'e21':0, 'e22':0, 'e23':1,
-             'e31':-1, 'e32':0, 'e33':0,
-             'dx':0, 'dy':0, 'dz':0,
-             'w_bn_b_x':0, 'w_bn_b_y':omega0, 'w_bn_b_z':0,
-             'ddelta':omega0,
-             'cos_delta':1, 'sin_delta':0,
-             'aileron':0, 'elevator':0, 'rudder':0, 'flaps':0,
-             'daileron':0, 'delevator':0, 'drudder':0, 'dflaps':0,
-             'nu':100, 'motor_torque':0,
-             'dmotor_torque':0, 'ddr':0,
-             'dddr':0.0, 'w0':0.0}
-    dotGuess = {'x':0, 'y':0, 'z':0, 'dx':0, 'dy':0, 'dz':0,
-                'r':0, 'dr':0,
-                'e11':0, 'e12':0, 'e13':0,
-                'e21':0, 'e22':0, 'e23':0,
-                'e31':0, 'e32':0, 'e33':0,
-                'w_bn_b_x':0, 'w_bn_b_y':0, 'w_bn_b_z':0,
-                'ddelta':0,
-                'cos_delta':0, 'sin_delta':omega0,
-                'aileron':0, 'elevator':0, 'rudder':0, 'flaps':0,
-                'motor_torque':0, 'ddr':0}
+    if guess is None:
+        guess = {'x':r0, 'y':0, 'z':0,
+                 'r':r0, 'dr':0,
+                 'e11':0, 'e12':-1, 'e13':0,
+                 'e21':0, 'e22':0, 'e23':1,
+                 'e31':-1, 'e32':0, 'e33':0,
+                 'dx':0, 'dy':0, 'dz':0,
+                 'w_bn_b_x':0, 'w_bn_b_y':omega0, 'w_bn_b_z':0,
+                 'ddelta':omega0,
+                 'cos_delta':1, 'sin_delta':0,
+                 'aileron':0, 'elevator':0, 'rudder':0, 'flaps':0,
+                 'daileron':0, 'delevator':0, 'drudder':0, 'dflaps':0,
+                 'nu':100, 'motor_torque':0,
+                 'dmotor_torque':0, 'ddr':0,
+                 'dddr':0.0, 'w0':0.0}
+    if dotGuess is None:
+        dotGuess = {'x':0, 'y':0, 'z':0, 'dx':0, 'dy':0, 'dz':0,
+                    'r':0, 'dr':0,
+                    'e11':0, 'e12':0, 'e13':0,
+                    'e21':0, 'e22':0, 'e23':0,
+                    'e31':0, 'e32':0, 'e33':0,
+                    'w_bn_b_x':0, 'w_bn_b_y':0, 'w_bn_b_z':0,
+                    'ddelta':0,
+                    'cos_delta':0, 'sin_delta':omega0,
+                    'aileron':0, 'elevator':0, 'rudder':0, 'flaps':0,
+                    'motor_torque':0, 'ddr':0}
 
     guessVec = C.DMatrix([guess[n] for n in dae.xNames() + dae.zNames() + dae.uNames() + dae.pNames()] +
                          [dotGuess[n] for n in dae.xNames()])
 
-    bounds = {'x':(-0.1 * r0, r0 * 2), 'y':(-1.1 * r0, 1.1 * r0), 'z':(-1.1 * r0, 1.1 * r0),
-             'dx':(0, 0), 'dy':(0, 0), 'dz':(0, 0),
-             'r':(r0, r0), 'dr':(-100, 100),
-             'e11':(-2, 2), 'e12':(-2, 2), 'e13':(-2, 2),
-             'e21':(-2, 2), 'e22':(-2, 2), 'e23':(-2, 2),
-             'e31':(-2, 2), 'e32':(-2, 2), 'e33':(-2, 2),
-             'w_bn_b_x':(-50, 50), 'w_bn_b_y':(-50, 50), 'w_bn_b_z':(-50, 50),
-             'ddelta':(omega0, omega0),
-             'cos_delta':(1, 1), 'sin_delta':(0, 0),
-             'aileron':(-0.2, 0.2), 'elevator':(-0.2, 0.2), 'rudder':(-0.2, 0.2), 'flaps':(-0.2, 0.2),
-             'daileron':(0, 0), 'delevator':(0, 0), 'drudder':(0, 0), 'dflaps':(0, 0),
-             'nu':(0, 3000), 'motor_torque':(-1000, 1000),
-             'ddr':(0, 0),
-             'dmotor_torque':(0, 0), 'dddr':(0, 0), 'w0':(0, 0)}
+    if bounds is None:
+        bounds = {'x':(-0.1 * r0, r0 * 2), 'y':(-1.1 * r0, 1.1 * r0), 'z':(-1.1 * r0, 1.1 * r0),
+                 'dx':(0, 0), 'dy':(0, 0), 'dz':(0, 0),
+                 'r':(r0, r0), 'dr':(-100, 100),
+                 'e11':(-2, 2), 'e12':(-2, 2), 'e13':(-2, 2),
+                 'e21':(-2, 2), 'e22':(-2, 2), 'e23':(-2, 2),
+                 'e31':(-2, 2), 'e32':(-2, 2), 'e33':(-2, 2),
+                 'w_bn_b_x':(-50, 50), 'w_bn_b_y':(-50, 50), 'w_bn_b_z':(-50, 50),
+                 'ddelta':(omega0, omega0),
+                 'cos_delta':(1, 1), 'sin_delta':(0, 0),
+                 'aileron':(-0.2, 0.2), 'elevator':(-0.2, 0.2), 'rudder':(-0.2, 0.2), 'flaps':(-0.2, 0.2),
+                 'daileron':(0, 0), 'delevator':(0, 0), 'drudder':(0, 0), 'dflaps':(0, 0),
+                 'nu':(0, 3000), 'motor_torque':(-1000, 1000),
+                 'ddr':(0, 0),
+                 'dmotor_torque':(0, 0), 'dddr':(0, 0), 'w0':(0, 0)}
 
     if ref_dict is not None:
         for name in ref_dict: bounds[name] = ref_dict[name]
 
 #     print bounds
 
-    dotBounds = {'x':(-1, 1), 'y':(-1, 1), 'z':(-1, 1),
-                 'dx':(0, 0), 'dy':(0, 0), 'dz':(0, 0),
-                 'r':(-100, 100), 'dr':(-1, 1),
-                 'e11':(-50, 50), 'e12':(-50, 50), 'e13':(-50, 50),
-                 'e21':(-50, 50), 'e22':(-50, 50), 'e23':(-50, 50),
-                 'e31':(-50, 50), 'e32':(-50, 50), 'e33':(-50, 50),
-                 'w_bn_b_x':(0, 0), 'w_bn_b_y':(0, 0), 'w_bn_b_z':(0, 0),
-                 'ddelta':(0, 0),
-                 'cos_delta':(-1, 1), 'sin_delta':(omega0 - 1, omega0 + 1),
-                 'aileron':(-1, 1), 'elevator':(-1, 1), 'rudder':(-1, 1), 'flaps':(-1, 1),
-                 'motor_torque':(-1000, 1000), 'ddr':(-100, 100)}
+    if dotBounds is None:
+        dotBounds = {'x':(-1, 1), 'y':(-1, 1), 'z':(-1, 1),
+                     'dx':(0, 0), 'dy':(0, 0), 'dz':(0, 0),
+                     'r':(-100, 100), 'dr':(-1, 1),
+                     'e11':(-50, 50), 'e12':(-50, 50), 'e13':(-50, 50),
+                     'e21':(-50, 50), 'e22':(-50, 50), 'e23':(-50, 50),
+                     'e31':(-50, 50), 'e32':(-50, 50), 'e33':(-50, 50),
+                     'w_bn_b_x':(0, 0), 'w_bn_b_y':(0, 0), 'w_bn_b_z':(0, 0),
+                     'ddelta':(0, 0),
+                     'cos_delta':(-1, 1), 'sin_delta':(omega0 - 1, omega0 + 1),
+                     'aileron':(-1, 1), 'elevator':(-1, 1), 'rudder':(-1, 1), 'flaps':(-1, 1),
+                     'motor_torque':(-1000, 1000), 'ddr':(-100, 100)}
     boundsVec = [bounds[n] for n in dae.xNames() + dae.zNames() + dae.uNames() + dae.pNames()] + \
                 [dotBounds[n] for n in dae.xNames()]
 
@@ -164,8 +169,9 @@ def getSteadyState(dae, conf, omega0, r0, ref_dict = {}):
 #    addCallback()
     solver.setOption('max_iter', 10000)
 #    solver.setOption('tol',1e-14)
-#    solver.setOption('suppress_all_output','yes')
-#    solver.setOption('print_time',False)
+    if verbose is False:
+        solver.setOption('suppress_all_output','yes')
+        solver.setOption('print_time',False)
     solver.init()
 
     solver.setInput(g.getLb(), 'lbg')
